368de0872692bd237059ac1af387dc90ed0bfe92
wycj2009 <wycj2009@naver.com>
2023-01-03 09:14:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android_tiledimageview_example/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android_tiledimageview_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_tiledimageview_example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
